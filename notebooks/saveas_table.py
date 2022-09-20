b810e42d0b88d45d9771f4ef0df133ed8e01f8a8
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2022-09-20 15:51:48 +0200

    Update saveas_table.py

--- notebooks/saveas_table.py
+++ notebooks/saveas_table.py
@@ -3,3 +3,10 @@ df = spark.read.load("/databricks-datasets/learning-spark-v2/people/people-10m.d
 # Write the data to a table.
 table_name = "people_10m"
 df.write.saveAsTable(table_name)
+
+# Describe Saved Table
+display(spark.sql('DESCRIBE DETAIL people_10m'))
+
+# Read Table
+people_df = spark.read.table(table_name)
+display(people_df)
