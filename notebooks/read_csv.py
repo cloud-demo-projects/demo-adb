8671c60563ffd31bbe2ec88e07b007396da3e043
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2022-09-23 14:22:55 +0200

    Update read_csv.py

--- notebooks/read_csv.py
+++ notebooks/read_csv.py
@@ -1,4 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Import csv and create table
+
+# COMMAND ----------
+
 import pandas as pd
+
+# COMMAND ----------
 
 test_pd = pd.read_csv("test.csv", sep='|', delimiter=None)
 test_pd = spark.createDataFrame(test_pd)
